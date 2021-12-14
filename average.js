70f2efb7f3ad0bf16a4e260092fc01213a042cea
pjnichal <nichal.pravin2001@gmail.com>
2021-12-14 10:55:37 +0530

    first

--- average.js
+++ average.js
@@ -1,6 +1,7 @@
-
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  return (
+    numbers.reduce((p, c) => (c.isNaN ? p + 0 : p + c), 0) / numbers.length
+  );
 }
 
-module.exports = {average};
+module.exports = { average };
