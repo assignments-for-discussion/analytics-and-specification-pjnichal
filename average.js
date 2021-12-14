8b5a10f8614ae67d7366e5d51bfabeb58d62d1ef
pjnichal <nichal.pravin2001@gmail.com>
2021-12-14 11:02:06 +0530

    second

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 function average(numbers) {
   return (
-    numbers.reduce((p, c) => (c.isNaN ? p + 0 : p + c), 0) / numbers.length
+    numbers.reduce((p, c) => {
+      return isNaN(c) ? p + 0 : p + c;
+    }, 0) / numbers.length
   );
 }
 
